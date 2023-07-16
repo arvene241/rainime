0f0795c808414e0d9c3094c8f2c316480eeae4c5
arvene <arvene241@gmail.com>
2023-07-16 23:11:03 +0800

    new type

--- lib/types.ts
+++ lib/types.ts
@@ -5,10 +5,28 @@ interface Title {
   userPreferred: string;
 }
 
+interface Genre {
+  action: string;
+  comedy: string;
+  horror: string;
+}
+
 export interface AnimeResult {
   id: string;
   title: Title;
   image: string;
+  type: string;
+  rating: number;
+  releaseDate: string;
+}
+
+export interface AnimeTrending {
+  id: string;
+  title: Title;
+  description: string;
+  cover: string;
+  image: string;
+  genre: Generator;
   type: string;
   rating: number;
   releaseDate: string;
