e7ab69ce769f668fa83f2b609b69fd57775828e0
arvene <arvene241@gmail.com>
2023-07-18 21:36:18 +0800

    add custom hook

---
+++ lib/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+interface ApiResponse<T> {
+  data: T;
+}
+
+export const useFetch = <ApiResponse>({ url }: {url: string}) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<ApiResponse | null>(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!url) return;
+
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError('Something went wront while fetching the data. Go to console to see the message');
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+
+  return { loading, error, data };
+}
--- lib/types.ts
+++ lib/types.ts
@@ -11,6 +11,17 @@ interface Genre {
   horror: string;
 }
 
+interface Recommendations {
+  id: string;
+  title: Title;
+  status: string;
+  episodes: number;
+  image: string;
+  cover: string;
+  rating: number;
+  type: string;
+}
+
 export interface AnimeResult {
   id: string;
   title: Title;
@@ -26,8 +37,41 @@ export interface AnimeTrending {
   description: string;
   cover: string;
   image: string;
-  genre: Generator;
+  genres: Genre;
   type: string;
   rating: number;
   releaseDate: string;
+}
+
+export interface RecentAnime {
+  id: string;
+  title: Title;
+  image: string;
+  genres: Genre;
+  type: string;
+  rating: number;
+  episodeId: string;
+  episodeNumber: number;
+  episodeTitle: string;
+}
+
+export interface AnimeInfo {
+  id: string;
+  title: Title;
+  image: string;
+  popularity: number;
+  cover: string;
+  descripition: string;
+  status: string;
+  genres: Genre;
+  totalEpisodes: number;
+  currentEpisodes: number;
+  type: string;
+  rating: number;
+  subOrDub: string;
+  recommendations: Recommendations[];
+}
+
+export interface Results<T> {
+  results: T[];
 }
