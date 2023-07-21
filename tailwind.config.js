e8629e09fc07ce28ee999f9d177505644195d7a4
arvene <arvene241@gmail.com>
2023-07-21 23:30:36 +0800

    add breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
       },
     },
     extend: {
+      screens: {
+        "mdl": "860px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
